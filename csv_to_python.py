b6204ea2b9bb7d7348c7c18dc1570085b8b55f4c
Paul Rios <paul.rios@utec.edu.pe>
2023-05-01 21:52:51 -0500

    v2

--- csv_to_python.py
+++ csv_to_python.py
@@ -8,8 +8,14 @@ import glob
 path = os.getcwd()
 csv_files = glob.glob(os.path.join(path, "*.csv"))
 
+index = [x/100 for x in range(0, 614)]
+
+
 df_fusion = pd.DataFrame()
 
+list1 = ["antebrazo", "hombro", "muneca"]
+df_fusion["time"] = index
+i=0
 # loop over the list of csv files
 for f in csv_files:
     # read the csv file
@@ -18,24 +24,22 @@ for f in csv_files:
     df = df.drop(df.columns[[0, 1, 6, 7, 8, 9]], axis=1)
 
     # Add 'index' column to both DataFrames
-    df['index'] = df.index
-
     # Initialize temp DataFrame with index column
     temp = pd.DataFrame()
-    temp['index'] = df.index
-
-    # Merge temp and df DataFrames on 'index' column
-    temp = pd.merge(temp, df, on='index', how='outer')
-
-    # Check if df_fusion is empty and if so, set it to temp
+    df['Quat_W'] = df['Quat_W'].astype(str)
+    df['Quat_X'] = df['Quat_X'].astype(str)
+    df['Quat_Y'] = df['Quat_Y'].astype(str)
+    df['Quat_Z'] = df['Quat_Z'].astype(str)
+    temp[list1[i]] = df[['Quat_W', 'Quat_X', 'Quat_Y', 'Quat_Z']].apply(lambda x: ','.join(x), axis=1)
+    i+=1
     if df_fusion.empty:
         df_fusion = temp
     else:
         # Merge df_fusion and temp DataFrames
-        df_fusion = pd.merge(df_fusion, temp, on='index', how='outer')
+        df_fusion = pd.concat([df_fusion, temp], axis=1)
 
 # Remove the 'index' column if not needed
-df_fusion = df_fusion.drop('index', axis=1)
+# df_fusion = df_fusion.drop('index', axis=1)
 
 # Prepare the header string
 header = "Coordinates\nnRows={}\nnColumns={}\nendheader\n".format(df_fusion.shape[0], df_fusion.shape[1])
